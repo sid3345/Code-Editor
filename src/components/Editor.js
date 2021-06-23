53e7d46d72f938d2feec92f76a28b51f517d2b71
Siddharth Sinha <sid3345@gmail.com>
2021-06-24 02:47:14 +0530

    integrated ace-editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,50 +1,58 @@
-import React, { useState } from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import { Controlled as ControlledEditor } from 'react-codemirror2'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
-export default function Editor(props) {
-  const {
-    language,
-    displayName,
-    value,
-    onChange
-  } = props
-  const [open, setOpen] = useState(true)
+// Import AceEditor Component
 
-  function handleChange(editor, data, value) {
-    onChange(value)
+import AceEditor from "react-ace";
+
+// Import a Mode (language)
+import 'brace/mode/java';
+
+// Import a Theme (okadia, github, xcode etc)
+import 'brace/theme/tomorrow';
+
+export default class Editor extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = { open: true }
+    this.onChange = this.onChange.bind(this);
   }
 
-  return (
-    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
-      <div className="editor-title">
-        {displayName}
-        <button
-          type="button"
-          className="expand-collapse-btn"
-          onClick={() => setOpen(prevOpen => !prevOpen)}
-        >
-          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
-        </button>
+  onChange(newValue) {
+    console.log('change', newValue);
+  }
+
+  render() {
+    return (
+      <div className={`editor-container ${this.state.open ? '' : 'collapsed'}`}>
+        <div className="editor-title">
+          {this.props.displayName}
+          <button
+            type="button"
+            className="expand-collapse-btn"
+            onClick={() => this.setState({ open: !this.state.open })}
+          >
+            <FontAwesomeIcon icon={this.state.open ? faCompressAlt : faExpandAlt} />
+          </button>
+        </div>
+        <AceEditor
+          mode={this.props.language}
+          theme="tomorrow"
+          onChange={this.props.onChange}
+          name="UNIQUE_ID_OF_DIV"
+          value={this.props.value}
+          defaultValue=''
+          editorProps={{ $blockScrolling: true }}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            highlightActiveLine: true
+          }}
+        />,
       </div>
-      <ControlledEditor
-        onBeforeChange={handleChange}
-        value={value}
-        className="code-mirror-wrapper"
-        options={{
-          lineWrapping: true,
-          lint: true,
-          mode: language,
-          theme: 'material',
-          lineNumbers: true
-        }}
-      />
-    </div>
-  )
+    );
+  }
 }
